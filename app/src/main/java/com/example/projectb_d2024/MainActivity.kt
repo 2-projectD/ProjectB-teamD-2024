67efd4df3f86454cb24dee2ea2a1c7ff441c2a43
NGO-079 <146411811+NGO-079@users.noreply.github.com>
2024-08-12 22:12:28 +0900

    Create MainActivity.kt
    設定

---
+++ app/src/main/java/com/example/projectb_d2024/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.projectb_d2024
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //1) Viewの取得
+        val btnStrat : Button = findViewById(R.id.btnStart)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        btnStrat.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
