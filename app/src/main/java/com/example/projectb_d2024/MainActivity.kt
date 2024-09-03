a73d656d224e699c8cdfe32fa25e4fe0a3542582
NGO-079 <c3p31079@bunkyo.ac.jp>
2024-09-04 03:22:29 +0900

    連絡ボタンへの移動プログラム

--- app/src/main/java/com/example/projectb_d2024/MainActivity.kt
+++ app/src/main/java/com/example/projectb_d2024/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.projectb_d2024
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +21,16 @@ class MainActivity : AppCompatActivity() {
         //val intent = Intent(this,遷移先::class.java)
         btnStrat.setOnClickListener{
             val intent = Intent(this, VoiceActivity::class.java)
+            startActivity(intent)
+        }
+
+        //1) Viewの取得(連絡ボタン)
+        val Contact : ImageButton = findViewById(R.id.Contact)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        Contact.setOnClickListener{
+            val intent = Intent(this, ContactActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/projectb_d2024/VoiceActivity.kt
+++ app/src/main/java/com/example/projectb_d2024/VoiceActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projectb_d2024
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
@@ -20,6 +21,16 @@ class VoiceActivity : AppCompatActivity() {
         //3)戻るボタン（アクティビティの終了）
         Back.setOnClickListener{
             finish()
+        }
+
+        //1) Viewの取得(連絡ボタン)
+        val Contact : ImageButton = findViewById(R.id.Contact)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        Contact.setOnClickListener{
+            val intent = Intent(this, ContactActivity::class.java)
+            startActivity(intent)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
