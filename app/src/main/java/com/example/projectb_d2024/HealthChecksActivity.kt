90c5d6537eb80e3c8bf8347f429f2e5350f1d2ac
NGO-079 <c3p31079@bunkyo.ac.jp>
2024-09-05 22:04:14 +0900

    健康チェック画面の作成

---
+++ app/src/main/java/com/example/projectb_d2024/HealthChecksActivity.kt
@@ -0,0 +1,40 @@
+package com.example.projectb_d2024
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HealthChecksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_health_checks)
+
+        val back : ImageButton = findViewById(R.id.back)
+
+        //3)戻るボタン（アクティビティの終了）
+        back.setOnClickListener{
+            finish()
+        }
+
+        //1) Viewの取得(連絡ボタン)
+        val contact : ImageButton = findViewById(R.id.contact)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        contact.setOnClickListener{
+            val intent = Intent(this, ContactActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
