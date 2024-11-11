4d9f4d88c40550f359f054d1507c43ace185b634
NGO-079 <c3p31079@bunkyo.ac.jp>
2024-11-12 02:57:17 +0900

    エラー修正

---
+++ app/src/main/java/com/example/projectb_d2024/Map_3f_Activity.kt
@@ -0,0 +1,18 @@
+package com.example.projectb_d2024
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Map_3f_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_map3f)
+
+
+
+    }
+}
