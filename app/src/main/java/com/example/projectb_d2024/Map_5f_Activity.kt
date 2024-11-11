44bb4dc3522bd61d2412d7ac6f89ed1324e254a9
NGO-079 <c3p31079@bunkyo.ac.jp>
2024-11-12 04:15:17 +0900

    mapの細かい調整

--- app/src/main/java/com/example/projectb_d2024/Map_5f_Activity.kt
+++ app/src/main/java/com/example/projectb_d2024/Map_5f_Activity.kt
@@ -1,6 +1,8 @@
 package com.example.projectb_d2024
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,45 @@ class Map_5f_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_map5f)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //1) Viewの取得(3Fボタン)
+        val imageButton5 : ImageButton = findViewById(R.id.imageButton5)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        imageButton5.setOnClickListener{
+            val intent = Intent(this, Map_5f_Activity::class.java)
+            startActivity(intent)
+        }
+
+        //1) Viewの取得(4Fボタン)
+        val imageButton6 : ImageButton = findViewById(R.id.imageButton6)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        imageButton6.setOnClickListener{
+            val intent = Intent(this, Map_4f_Activity::class.java)
+            startActivity(intent)
+        }
+
+        //1) Viewの取得(1Fボタン)
+        val imageButton3 : ImageButton = findViewById(R.id.imageButton3)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        imageButton3.setOnClickListener{
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
+
+        //1) Viewの取得(メイン画面へボタン)
+        val imageButton : ImageButton = findViewById(R.id.imageButton)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        imageButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
