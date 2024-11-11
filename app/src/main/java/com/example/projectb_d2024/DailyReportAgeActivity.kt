95d300f5f21e41f7b9391cbb83c42b9a47e7a38d
NGO-079 <c3p31079@bunkyo.ac.jp>
2024-11-12 03:30:37 +0900

    DailyReportActivityへ移動ボタン

--- app/src/main/java/com/example/projectb_d2024/DailyReportAgeActivity.kt
+++ app/src/main/java/com/example/projectb_d2024/DailyReportAgeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.projectb_d2024
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,54 @@ class DailyReportAgeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_daily_report_age)
+
+        val back : ImageButton = findViewById(R.id.back)
+
+        //1) Viewの取得(連絡ボタン)
+        val contact : ImageButton = findViewById(R.id.contact)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        contact.setOnClickListener{
+            val intent = Intent(this, ContactActivity::class.java)
+            startActivity(intent)
+        }
+
+        //1) Viewの取得(メニュー画面へボタン)
+        val btnMenu : ImageButton = findViewById(R.id.btnMenu)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        btnMenu.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        //1) Viewの取得(メニュー画面へボタン)
+        val healthCheck : ImageButton = findViewById(R.id.healthCheck)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        healthCheck.setOnClickListener{
+            val intent = Intent(this, DailyReportActivity::class.java)
+            startActivity(intent)
+        }
+
+        //1) Viewの取得(声の画面へボタン)
+        val btnVoice : ImageButton = findViewById(R.id.btnVoice)
+
+        //2) ボタンを押したら次の画面へ
+        //val intent = Intent(this,遷移先::class.java)
+        btnVoice.setOnClickListener{
+            val intent = Intent(this, VoiceActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 戻るボタン
+        back.setOnClickListener {
+            finish()
+        }
 
     }
 }
